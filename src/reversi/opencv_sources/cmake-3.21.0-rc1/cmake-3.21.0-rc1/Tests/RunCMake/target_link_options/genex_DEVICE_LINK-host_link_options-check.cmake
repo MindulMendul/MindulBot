6cf69fdc2a4e421c30f86d8810a7feb850318e7f
Mindul <pencake33@naver.com>
2021-06-30 12:36:03 +0900

    5

--- src/reversi/opencv_sources/cmake-3.21.0-rc1/cmake-3.21.0-rc1/Tests/RunCMake/target_link_options/genex_DEVICE_LINK-host_link_options-check.cmake
+++
@@ -1,4 +0,0 @@
-
-if (NOT actual_stdout MATCHES "-Xlinker=OPT1 -Xlinker=OPT2 -Xlinker=OPT3 -Xlinker=OPT4 -Xlinker=OPT5")
-    set (RunCMake_TEST_FAILED "Not found expected '-Xlinker=OPT1 -Xlinker=OPT2 -Xlinker=OPT3 -Xlinker=OPT4 -Xlinker=OPT5'.")
-endif()
