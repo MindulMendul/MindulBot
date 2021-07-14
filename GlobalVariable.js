f645826b040530922245e1166428a5d26a3d8194
mindul <pencake33@naver.com>
2021-07-14 18:47:48 +0900

    봇 모르모트 로그인 기능 간편화

--- GlobalVariable.js
+++ GlobalVariable.js
@@ -3,12 +3,14 @@ const MORMOTTE_ID="751773063766343721";
 const OWNER_ID="554178159717777420";
 
 //봇 로그인 //테섭과 본섭 로그인 구별
-let Token="M", ID=Token;
-if(Token="M")//모르모트 로그인
-{Token=process.env.MORMOTTE_TOKEN;   ID=MORMOTTE_ID;}
-else if (login="B")//봇 로그인
-{Token=process.env.MORMOTTE_TOKEN;   ID=MORMOTTE_ID;}
-
-const LoginBotToken=Token; exports.LoginBotToken=LoginBotToken; 
-const LoginBotID=ID; exports.LoginBotID=LoginBotID;
-const PREFIX="ㅏ"; exports.PREFIX=PREFIX;
+const Token="BOT";
+if (Token=="BOT") {//봇 로그인
+    exports.LoginBotToken=process.env.BOT_TOKEN;
+    exports.LoginBotID=BOT_ID;
+    const PREFIX="ㅣ"; exports.PREFIX=PREFIX;
+}
+else{//모르모트 로그인
+    exports.LoginBotToken=process.env.MORMOTTE_TOKEN;
+    exports.LoginBotID=MORMOTTE_ID;
+    const PREFIX="ㅏ"; exports.PREFIX=PREFIX;
+}
