69e437d4b1dd9d297b406df6039f7d8651ceeec9
mindul <pencake33@naver.com>
2021-07-14 18:47:48 +0900

    봇 모르모트 로그인 기능 간편화

---
+++ GlobalVariable.js
@@ -0,0 +1,16 @@
+const BOT_ID="751733763838443530";
+const MORMOTTE_ID="751773063766343721";
+const OWNER_ID="554178159717777420";
+
+//봇 로그인 //테섭과 본섭 로그인 구별
+const Token="BOT";
+if (Token=="BOT") {//봇 로그인
+    exports.LoginBotToken=process.env.BOT_TOKEN;
+    exports.LoginBotID=BOT_ID;
+    const PREFIX="ㅣ"; exports.PREFIX=PREFIX;
+}
+else{//모르모트 로그인
+    exports.LoginBotToken=process.env.MORMOTTE_TOKEN;
+    exports.LoginBotID=MORMOTTE_ID;
+    const PREFIX="ㅏ"; exports.PREFIX=PREFIX;
+}
