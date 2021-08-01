3b310ef2b72e789b67567ac88e604d728e7d4446
mindul <pencake33@naver.com>
2021-08-02 01:31:19 +0900

    서버 버그 고치는 중3

--- GlobalVariable.js
+++ GlobalVariable.js
@@ -1,18 +1,14 @@
-const BOT_ID="751733763838443530";
-const MORMOTTE_ID="751773063766343721";
 exports.OWNER_ID="554178159717777420";
 
 //봇 로그인 //테섭과 본섭 로그인 구별
 const Token="MORMOTTE";
 if (Token=="BOT") {//봇 로그인
     exports.LoginBotToken=process.env.BOT_TOKEN;
-    exports.LoginBotID=BOT_ID;
     exports.PREFIX="ㅣ";
     exports.activityString="성적에서 F만 피";
 }
 else{//모르모트 로그인
     exports.LoginBotToken=process.env.MORMOTTE_TOKEN;
-    exports.LoginBotID=MORMOTTE_ID;
     exports.PREFIX="ㅏ";
     exports.activityString="테스트";
 }
