974397235f5e52b6e7bbb82f3556175c1dbaa548
mindul <pencake33@naver.com>
2021-08-05 14:45:18 +0900

    노래봇 버전확인 버그 수정

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ bot.on('message', async (msg) => {
 		const checkGuildCmdQueue=bot.guildCmdQueue.get(`${msg.guild.id}${bot.commands.get(command).type}`);
 	try {
 		if(bot.commands.get(command).type=="music"){//노래봇은 최신 버전이어야만 쓸 수 있음
-			const tmp=await require("./src/Commands/music/musicVerCheck").verCheck(); if(tmp) return;}
+			const tmp=await require("./src/Commands/music/musicVerCheck").verCheck(bot, msg); if(tmp) return;}
 
 		if(checkGuildCmdQueue.length==0){ //아무것도 실행 안 되어 있으면 실행
 			checkGuildCmdQueue.push(bot.commands.get(command));//명령어 입력 중임을 알림
