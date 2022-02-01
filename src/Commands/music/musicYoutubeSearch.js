59b0d3480ae6aee9aafab3fcb80ec254cee123c8
Mindul <pencake33@naver.com>
2022-02-01 21:54:28 +0900

    filter 부분 버그 고침

--- src/Commands/music/musicYoutubeSearch.js
+++ src/Commands/music/musicYoutubeSearch.js
@@ -40,25 +40,23 @@ module.exports = {
             embedSearchYoutube.fields.push(explItem);
         }
         const embedMsg = await msg.channel.send({embeds: [embedSearchYoutube]});
-        this.react(embedSearchYoutube, embedMsg);
-    }, 
-    async react(embed, embedMsg){
-        const msgFilter = (msg) => {return !(msg.author.bot);}
-        const collector = embedMsg.channel.createMessageCollector({msgFilter, max: 1});
-        collector.on('collect', async (msg) => {
-            const msgArr=await func.effectiveArr(msg.content, ",", 1, 8);//배열이 유효한지 조사
+
+        const filter = (message) => {return (!message.author.bot)&(message.author.id===msg.author.id);};
+        const collector = msg.channel.createMessageCollector({filter, max: 1});
+        collector.on('collect', async (message) => {
+            const msgArr=await func.effectiveArr(message.content, ",", 1, 8);//배열이 유효한지 조사
 
             if(msgArr.length==0){ //리스트에 추가할 게 없을 때(즉, 검색이 유효하지 않으면 바로 취소함)
-                check=true; msg.delete(); embedMsg.delete();
-                return msg.channel.send("유효하지 않은 대답이에요. 노래 검색 취소할게요..;;");
+                check=true; message.delete(); embedMsg.delete();
+                return message.channel.send("유효하지 않은 대답이에요. 노래 검색 취소할게요..;;");
             }
 
             while(msgArr.length>0){
-                const tmpStr=embed.fields[msgArr.shift()].url.split(/\s+/);
-                await require("./musicExecute").execute(msg, tmpStr);
+                const tmpStr=embedSearchYoutube.fields[msgArr.shift()].url.split(/\s+/);
+                await require("./musicExecute").execute(message, tmpStr);
             }
-            msg.delete();
+            message.delete();
             embedMsg.delete();
         });
-    }
+    },
 };
