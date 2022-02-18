fc2a58e1172f7f2ba79a9aaf185a84c09332d6db
Mindul <pencake33@naver.com>
2022-02-18 16:56:15 +0900

    다른 보이스채널에서 커멘드 입력 막기 업데이트

--- src/Commands/music/musicRemove.js
+++ src/Commands/music/musicRemove.js
@@ -11,12 +11,11 @@ module.exports = {
             return msg.channel.send("보이스채널에서 해주세요!");
         
         const connection = getVoiceConnection(msg.guild.id);
-        if(connection==undefined) {
+        if(!connection)
             return msg.channel.send("재생목록에 노래가 없어요!");
-        }
 
-        //if (msg.member.voice.channel!=serverQueue.voiceChannel)
-        //    return msg.channel.send("같은 보이스채널에서 해주세요!");
+        if(msg.member.voice.channelId!=connection.joinConfig.channelId)
+            return msg.channel.send("같은 보이스채널에서 해주세요!");
 
         const argsArr=await func.effectiveArr(args.toString(),",",1,connection.subscription.songs.length);//배열이 유효한지 조사
         if(argsArr.length==0)
