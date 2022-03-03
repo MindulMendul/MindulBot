4640438d800c5c09cd44d11b59698ec787456133
mindul <pencake33@naver.com>
2022-03-03 12:47:55 +0900

    .env 변수 정리 남은 거;;

--- bot.js
+++ bot.js
@@ -51,7 +51,6 @@ bot.on('messageCreate', async (msg) => {
 	if(await noCmd(msg)) return;//명령어 없는 텍스트
 	if(msg.channel.type==="DM") return msg.channel.send("DM은 막혀있어요, 죄송합니다. ㅠㅠ");
 	
-	console.log(msg.author.id);
 	const args = msg.content.slice(process.env.PREFIX.length).trim().split(/\s+/);//명령어 말 배열에 담기
 	const command = args.shift();//명령어 인식할 거
 	
