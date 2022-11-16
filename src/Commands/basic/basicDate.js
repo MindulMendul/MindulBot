8ca72ab0f1adef31c4ad4b50708890ccd24a7290
mindul <pencake33@naver.com>
2022-11-17 00:17:09 +0900

    진짜진짜 env 대응 완료

--- src/Commands/basic/basicDate.js
+++ src/Commands/basic/basicDate.js
@@ -5,9 +5,8 @@ module.exports = {
     permission: [""],
 	async execute(msg) {
         const moment = require('moment');
-        const {OWNER_ID} = require('../../../GlobalVariable');
         
-		if(msg.author.id===OWNER_ID) msg.reply(moment().format("오늘은 MM월 DD일(dddd) 입니다, 주인님."));
+		if(msg.author.id===process.env.OWNER_ID) msg.reply(moment().format("오늘은 MM월 DD일(dddd) 입니다, 주인님."));
         else msg.reply("날짜는 달력 찾아봐.");
         return "cmdFinished";
 	},
--- src/Commands/basic/basicSuggetion.js
+++ src/Commands/basic/basicSuggetion.js
@@ -5,10 +5,9 @@ module.exports = {
 	permission: [""],
 	execute(msg, args) {
 		const {bot}=require('../../../bot');
-		const {OWNER_ID} = require('../../../GlobalVariable');
 		const content=args.join(" ");
 
 		if(content=="") msg.channel.send("공백은 건의할 수 없어요. 정당한 사항을 건의해주세요!");
-		else bot.users.cache.get(OWNER_ID).send(`'${msg.guild.name}'길드의 '${msg.channel.name}'채널에서 '${msg.author.username}'님이 건의사항 보내주셨어요.\n> ${content}`);
+		else bot.users.cache.get(process.env.OWNER_ID).send(`'${msg.guild.name}'길드의 '${msg.channel.name}'채널에서 '${msg.author.username}'님이 건의사항 보내주셨어요.\n> ${content}`);
 	},
 };
