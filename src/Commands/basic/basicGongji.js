1e9bd6537587a4921a81b7f6723805c5318fdb1e
Mindul <pencake33@naver.com>
2022-11-17 00:17:08 +0900

    노래봇 고침(스킵후 사운드 적용)

--- src/Commands/basic/basicGongji.js
+++ src/Commands/basic/basicGongji.js
@@ -7,7 +7,8 @@ module.exports = {
         console.log("ㅎㅇ");
         const {bot}=require("./../../../bot");
 
-        const args=msg.content.trim().split(/\s*\/\s*/);
+        const args=msg.content.slice(3,msg.content.length).trim().split(/\s*\/\s*/);
+        console.log(args);
         bot.guilds.cache.find((guild) => {//길드 이름 찾기
             if (guild.name == args[0]) {
                 guild.channels.cache.find((channel) => {//서버 이름 찾기
