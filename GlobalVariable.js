f76fd82a0fcac655bfc2528a86b196c2207e230d
Mindul <pencake33@naver.com>
2022-11-17 00:17:08 +0900

    git에 문제있어서 재 업로드

--- GlobalVariable.js
+++ GlobalVariable.js
@@ -1,7 +1,7 @@
 exports.OWNER_ID="554178159717777420";
 
 //봇 로그인 //테섭과 본섭 로그인 구별
-const Token="BOㅇT";
+const Token="BOT";
 if (Token=="BOT") {//봇 로그인
     exports.LoginBotToken=process.env.BOT_TOKEN;
     exports.PREFIX="ㅣ";
