3d44a5eae772c1751ab017ffd32da76b26e303a8
mindul <pencake33@naver.com>
2022-11-17 00:17:11 +0900

    비우기 기능 수정

--- src/cmd/music/musicEmpty.ts
+++ src/cmd/music/musicEmpty.ts
@@ -11,13 +11,16 @@ export const musicEmpty: CMD = {
     const guildId = msg.guildId as string;
     const msgMember = msg.member as GuildMember;
     const textChannel = msg.channel as TextChannel;
-    const musicEntity = musicCollection.get(guildId);
-    
+    let musicEntity = musicCollection.get(guildId);
+
     if (!musicEntity) return textChannel.send('노래 명령어를 먼저 입력해주세요!');
     if (!msgMember.voice.channel) return textChannel.send('보이스채널에서 해주세요!');
     if (!musicEntity.connection) return textChannel.send('재생목록에 노래가 없어요!');
     if (msgMember.voice.channelId != musicEntity.voiceChannel.id)
       return textChannel.send('같은 보이스채널에서 해주세요!');
+
+    musicEntity.songQueue=[];
+    musicCollection.set(guildId, musicEntity);
 
     musicEntity.subscription?.player.stop();
   }
